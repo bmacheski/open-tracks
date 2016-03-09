d435920a4a9defda53d4efe8e358090321cd0c15
bmacheski <bwm0007@gmail.com>
2016-03-09 15:46:55 -0600

    Fix join on room with currently playing song bug

--- client/js/reducers/player.js
+++ client/js/reducers/player.js
@@ -1,13 +1,14 @@
 const initialState = {
   time: 0,
   songIndex: 0,
-  isPlaying: false
+  isPlaying: false,
+  isPaused: false
 }
 
 export default function player(state = initialState, action) {
   switch (action.type) {
   case 'UPDATE_CURRENT_PLAYER_TIME':
-    if (state.isPlaying) {
+    if (!state.isPaused) {
       return Object.assign({}, state, {
         time: action.time,
         isPlaying: true
@@ -18,18 +19,20 @@ export default function player(state = initialState, action) {
         time: action.time,
         isPlaying: false
       })
-   }
+    }
   case 'UPDATE_CURRENT_PLAYLIST_SONG':
     return Object.assign({}, state, {
       songIndex: action.songIndex
     })
   case 'SET_IS_PLAYING':
     return Object.assign({}, state, {
-      isPlaying: true
+      isPlaying: true,
+      isPaused: false
     })
   case 'SET_NOT_PLAYING':
     return Object.assign({}, state, {
-      isPlaying: false
+      isPlaying: false,
+      isPaused: true
     })
   default:
     return state
